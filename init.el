615a1fe312946d38558ccfdf131d6837c0df70dc
Hristo Yankov <ico.yankov@gmail.com>
2016-11-01 15:19:10 +0200

    Add swap meta & alt for mac.

--- init.el
+++ init.el
@@ -132,4 +132,20 @@ by Prelude.")
  ;; greet the use with some useful tip
  (run-at-time 5 nil 'prelude-tip-of-the-day))
 
+(defun swap-meta-and-super ()
+  "Swap the mapping of meta and super.
+Very useful for people using their Mac with a Windows external keyboard from time to time."
+  (interactive)
+  (if (eq mac-command-modifier 'super)
+      (progn
+        (setq mac-command-modifier 'meta)
+        (setq mac-option-modifier 'super)
+        (message "Command is now bound to META and Option is bound to SUPER."))
+    (progn
+      (setq mac-command-modifier 'super)
+      (setq mac-option-modifier 'meta)
+      (message "Command is now bound to SUPER and Option is bound to META."))))
+
+(global-set-key (kbd "C-c w") 'swap-meta-and-super)
+
 ;;; init.el ends here
